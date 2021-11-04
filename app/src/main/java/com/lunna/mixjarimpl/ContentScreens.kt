3620b43f6ba98400e571d39e8371e8beb81479e2
ianngech <ianngech@gmail.com>
2021-11-04 03:14:26 +0300

    structure added

---
+++ app/src/main/java/com/lunna/mixjarimpl/ContentScreens.kt
@@ -0,0 +1,72 @@
+package com.lunna.mixjarimpl
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.lunna.mixjarimpl.ui.theme.MixjarImplTheme
+
+
+@Composable
+fun TrendingScreen(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.primary)
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+            text = "Trending Screen",
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+            fontSize = 25.sp
+        )
+    }
+}
+
+@Preview(name = "trendingScreenDay")
+@Preview(name = "trendingScreenNight",uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun TrendingScreenPreview(){
+    MixjarImplTheme {
+        TrendingScreen()
+    }
+}@Composable
+fun GenericScreen(text:String){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.primary)
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+            text = text,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+            fontSize = 25.sp
+        )
+    }
+}
+
+@Preview(name = "genericScreenDay")
+@Preview(name = "genericScreenNight",uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun GenericScreenPreview(){
+    MixjarImplTheme {
+        GenericScreen("listens")
+    }
+}
+
--- app/src/main/java/com/lunna/mixjarimpl/MainActivity.kt
+++ app/src/main/java/com/lunna/mixjarimpl/MainActivity.kt
@@ -1,28 +1,21 @@
 package com.lunna.mixjarimpl
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.tooling.preview.Preview
 import com.lunna.mixjarimpl.ui.theme.MixjarImplTheme
-import com.mixsteroids.mixjar.MixCloud
-import com.mixsteroids.mixjar.models.TagResponse
-import com.mixsteroids.mixjar.models.UserResponse
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.Observer
+import androidx.navigation.*
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
 
@@ -33,7 +26,7 @@ class MainActivity : ComponentActivity() {
         mixjarViewModel.getTag()
         setContent {
             MixjarImplTheme {
-                TopBar()
+                MainScreen()
             }
         }
 
@@ -42,8 +35,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Navigation(navController:NavHostController){
+   NavHost(navController,startDestination = NavigationItem.Trending.route){
+       composable(NavigationItem.Trending.route){
+           TrendingScreen()
+       }
+       composable(NavigationItem.Listens.route){
+           GenericScreen("Listens")
+       }
+       composable(NavigationItem.Playlists.route){
+           GenericScreen("Playlists")
+       }
+       composable(NavigationItem.Profile.route){
+           GenericScreen("Profile")
+       }
+   }
 }
 
 @Composable
@@ -58,7 +64,74 @@ fun TopBar(){
     )
 }
 
-@Preview(showBackground = true)
+@Composable
+fun BottomNavigationBar(navController: NavController) {
+    val items = listOf(
+        NavigationItem.Trending,
+        NavigationItem.Listens,
+        NavigationItem.Playlists,
+        NavigationItem.Profile
+    )
+    BottomNavigation(
+        backgroundColor = MaterialTheme.colors.primary,
+        contentColor =  Color.White
+    ){
+        items.forEach { item ->
+            BottomNavigationItem(
+                icon = { Icon(item.icon, contentDescription = null)},
+                label = {Text(text = item.title)},
+                selectedContentColor = Color.White,
+                unselectedContentColor = Color.White.copy(0.4f),
+                alwaysShowLabel = true,
+                selected = false,
+                onClick = {
+                    navController.navigate(item.route){
+                        navController.graph.startDestinationRoute?.let { route ->
+                            popUpTo(route){
+                                saveState =true
+                            }
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+
+            )
+        }
+    }
+}
+
+@Composable
+fun MainScreen(){
+    val navController = rememberNavController()
+    Scaffold(
+        topBar = { TopBar()},
+        bottomBar = { BottomNavigationBar(navController)}
+    ) {
+        Navigation(navController)
+    }
+}
+@Composable
+@Preview(name = "mainScreenDay")
+@Preview(name = "mainScreenNight", uiMode = UI_MODE_NIGHT_YES)
+fun MainScreenPreview(){
+    MixjarImplTheme {
+        MainScreen()
+    }
+}
+
+@Preview(name = "dayBottomNav")
+@Preview(name = "nightBottomNav",uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun BottomNavigationPreview(){
+    MixjarImplTheme {
+//        BottomNavigationBar(navController)
+    }
+
+}
+
+@Preview(showBackground = true,
+uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun DefaultPreview() {
     MixjarImplTheme {
