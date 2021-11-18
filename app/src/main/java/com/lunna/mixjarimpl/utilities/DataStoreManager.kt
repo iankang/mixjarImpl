a3ce2e869db516b04656f72fea7fd28b7ad7057c
ianngech <ianngech@gmail.com>
2021-11-19 00:54:41 +0300

    added datastore changes

--- app/src/main/java/com/lunna/mixjarimpl/utilities/DataStoreManager.kt
+++ app/src/main/java/com/lunna/mixjarimpl/utilities/DataStoreManager.kt
@@ -2,44 +2,39 @@ package com.lunna.mixjarimpl.utilities
 
 import android.content.Context
 import androidx.datastore.core.DataStore
-import androidx.datastore.createDataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.clear
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.preferencesKey
-import androidx.datastore.preferences.createDataStore
+import androidx.datastore.preferences.core.*
+import androidx.datastore.preferences.preferencesDataStore
 import com.lunna.mixjarimpl.viewmodels.MixjarViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 
 const val USER_DATASTORE ="mixjar_datastore"
-class DataStoreManager(val context: Context) {
+class DataStoreManager(private val context: Context) {
 
     companion object {
-        private val USERNAME = preferencesKey<String>("username")
+        private val Context.dataStoree : DataStore<Preferences>  by   preferencesDataStore(
+            USER_DATASTORE)
+        val USERNAME = stringPreferencesKey("username")
     }
 
-    private val dataStore: DataStore<Preferences> = context.createDataStore(
-        name = USER_DATASTORE
-    )
+
 
     suspend fun saveToDataStore(username:String) {
-        dataStore.edit { preferences ->
+        context.dataStoree.edit { preferences ->
             preferences[USERNAME] = username
         }
     }
     suspend fun removeFromDataStore() {
 
-        dataStore.edit {preferences ->
+        context.dataStoree.edit {preferences ->
             preferences.clear()
         }
     }
 
-    val username: Flow<String> = dataStore.data
+    val username: Flow<String?> = context.dataStoree.data
         .map { preferences ->
-            val username = preferences[USERNAME] ?: ""
-            username
+            preferences[USERNAME]
         }
     //4
 
