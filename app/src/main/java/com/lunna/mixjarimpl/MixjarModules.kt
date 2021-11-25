d02cd09c449766564383cffce022cc68da75d631
ianngech <ianngech@gmail.com>
2021-11-25 19:36:29 +0300

    starting on db

--- app/src/main/java/com/lunna/mixjarimpl/MixjarModules.kt
+++ app/src/main/java/com/lunna/mixjarimpl/MixjarModules.kt
@@ -1,7 +1,23 @@
 package com.lunna.mixjarimpl
 
+import android.content.Context
+import androidx.room.Room
+import com.lunna.mixjarimpl.db.MixjarImplDB
+import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module
 
 val databaseModule = module {
+    fun provideDB(context: Context): MixjarImplDB {
+        synchronized(this){
+               return Room.databaseBuilder(
+                    context.applicationContext,
+                    MixjarImplDB::class.java,
+                    "mixjar_db"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+        }
+    }
 
+    single { provideDB(androidApplication()) }
 }
--- app/src/main/java/com/lunna/mixjarimpl/db/MixjarImplDB.kt
+++ app/src/main/java/com/lunna/mixjarimpl/db/MixjarImplDB.kt
@@ -9,21 +9,6 @@ abstract class MixjarImplDB: RoomDatabase() {
     companion object {
         private var INSTANCE: MixjarImplDB? = null
 
-        fun getInstance(context:Context):MixjarImplDB{
-            synchronized(this){
-                var instance = INSTANCE
 
-                if(instance == null){
-                    instance = Room.databaseBuilder(
-                        context.applicationContext,
-                        MixjarImplDB::class.java,
-                        "mixjar_db"
-                    )
-                        .fallbackToDestructiveMigration()
-                        .build()
-                }
-                return  instance
-            }
-        }
     }
 }
